7a93b6526064125edb072bae6e3032ad941f9add
Vladyslav <vladislavcherkashin98@gmail.com>
2021-11-15 15:21:47 +0200

    initial commit

---
+++ src/main/java/ee/hansabIT/CarsOwnerApplication.java
@@ -0,0 +1,13 @@
+package ee.hansabIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsOwnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsOwnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ee/hansabIT/CarsOwnerApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.hansabIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsOwnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
